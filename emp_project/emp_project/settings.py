c88820dcc015974eaf2266461854917874209305
Virendra patel <patelvirendra62@gmail.com>
2021-03-01 10:47:34 +0530

    iframe

--- emp_project/emp_project/settings.py
+++ emp_project/emp_project/settings.py
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'emp_project.urls'
